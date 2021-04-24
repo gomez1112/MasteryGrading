690f720fde6e62026e3f0dc0bcd4192ab61ddbe8
Gerard Gomez <gerardgomez11@me.comgerardgomez11@me.comgerardgomez11@me.com>
2021-04-24 18:00:20 -0400

    Create the beginning of the project

---
+++ MasteryGrading/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MasteryGrading
+//
+//  Created by Gerard Gomez on 4/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MasteryGrading/MasteryGradingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasteryGradingApp.swift
+//  MasteryGrading
+//
+//  Created by Gerard Gomez on 4/24/21.
+//
+
+import SwiftUI
+
+@main
+struct MasteryGradingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
